c1ffa80d700e10b09e359cdf711a96f3aea947c4
Alessandro Suglia <alessandro.suglia@gmail.com>
2020-07-21 20:26:54 +0100

    Moved boxes tensor to device

--- tools/gold_extractor_mscoco.sh
+++ tools/gold_extractor_mscoco.sh
@@ -3,4 +3,4 @@
 # first symlink inside the docker container
 python3 symlink_coco_images.py --image_dir /images/ -data_out /images/;
 
-python3 feature_extractor --image_root /images --images_metadata /images/metadata.json --gold_boxes --output_dir /images/gold_fastrcnn;
+python3 tools/feature_extractor.py --image_root /images --images_metadata /images/metadata.json --gold_boxes --output_dir /images/gold_fastrcnn;
