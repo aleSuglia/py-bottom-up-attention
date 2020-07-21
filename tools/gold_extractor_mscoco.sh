07c9d0e355cd633ef815fa064414015e3f3b81c3
Alessandro Suglia <alessandro.suglia@gmail.com>
2020-07-21 17:56:40 +0100

    Added symlink script to the mscoco extractor

--- tools/gold_extractor_mscoco.sh
+++ tools/gold_extractor_mscoco.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-python3 feature_extractor --image_root /images --images_metadata /images/metadata.json --gold_boxes --output_dir /images/gold_fastrcnn
+# first symlink inside the docker container
+python3 symlink_coco_images.py --image_dir /images/ -data_out /images/;
+
+python3 feature_extractor --image_root /images --images_metadata /images/metadata.json --gold_boxes --output_dir /images/gold_fastrcnn;
