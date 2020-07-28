cefb3eb11bd80ce9a6b9a1cfd96f959b710979be
Alessandro Suglia <alessandro.suglia@gmail.com>
2020-07-28 22:05:07 +0100

    Added correct probability scores to the instance class

--- tools/feature_extractor.py
+++ tools/feature_extractor.py
@@ -12,8 +12,9 @@ import tqdm
 
 from detectron2.config import get_cfg
 from detectron2.engine import DefaultPredictor
+from detectron2.layers import batched_nms
 from detectron2.modeling.postprocessing import detector_postprocess
-from detectron2.modeling.roi_heads.fast_rcnn import FastRCNNOutputs, fast_rcnn_inference_single_image
+from detectron2.modeling.roi_heads.fast_rcnn import FastRCNNOutputs
 from detectron2.structures import Boxes, Instances, BoxMode
 
 # import some common libraries
@@ -31,6 +32,55 @@ parser.add_argument("--score_threshold", type=float, help="Score threshold for t
 parser.add_argument("--gold_boxes", action="store_true",
                     help="Specify if you want to use gold bounding boxes instead of region proposals")
 args = parser.parse_args()
+
+
+def fast_rcnn_inference_single_image(
+        boxes, scores, image_shape, score_thresh, nms_thresh, topk_per_image
+):
+    """
+    Single-image inference. Return bounding-box detection results by thresholding
+    on scores and applying non-maximum suppression (NMS).
+
+    Args:
+        Same as `fast_rcnn_inference`, but with boxes, scores, and image shapes
+        per image.
+
+    Returns:
+        Same as `fast_rcnn_inference`, but for only one image.
+    """
+    class_distr_scores = scores.clone()
+    scores = scores[:, :-1]
+    num_bbox_reg_classes = boxes.shape[1] // 4
+    # Convert to Boxes to use the `clip` function ...
+    boxes = Boxes(boxes.reshape(-1, 4))
+    boxes.clip(image_shape)
+    boxes = boxes.tensor.view(-1, num_bbox_reg_classes, 4)  # R x C x 4
+
+    # Filter results based on detection scores
+    filter_mask = scores > score_thresh  # R x K
+    # R' x 2. First column contains indices of the R predictions;
+    # Second column contains indices of classes.
+    filter_inds = filter_mask.nonzero()
+    if num_bbox_reg_classes == 1:
+        boxes = boxes[filter_inds[:, 0], 0]
+    else:
+        boxes = boxes[filter_mask]
+    scores = scores[filter_mask]
+
+    # Apply per-class NMS
+    keep = batched_nms(boxes, scores, filter_inds[:, 1], nms_thresh)
+    if topk_per_image >= 0:
+        keep = keep[:topk_per_image]
+    boxes, scores, filter_inds = boxes[keep], scores[keep], filter_inds[keep]
+
+    result = Instances(image_shape)
+    result.pred_boxes = Boxes(boxes)
+    class_distr_scores = class_distr_scores[filter_inds[:, 0]]
+    # we set the background probability to 0
+    class_distr_scores[:, -1] = 0.0
+    result.scores = class_distr_scores
+    result.pred_classes = filter_inds[:, 1]
+    return result, filter_inds[:, 0]
 
 
 def extract_features(args, detector, raw_images, given_boxes=None):
